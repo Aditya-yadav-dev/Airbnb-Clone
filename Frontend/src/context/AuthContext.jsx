1ef358aa949b0f2f7d23e7cce4ce5720953e44eb
Aditya Yadav <aditya828326@gmail.com>
2025-08-04 19:18:23 +0530

    Update AuthContext.jsx

--- Frontend/src/context/AuthContext.jsx
+++ Frontend/src/context/AuthContext.jsx
@@ -5,7 +5,7 @@ export const AuthDataContext = createContext();
 const AuthContext = ({children}) => {
   const [loading, setloading] = useState(false)
   // const ServerUrl = 'http://localhost:3000';
-  const ServerUrl = 'https://airbnclone.onrender.com';
+  const ServerUrl = 'https://airbnbclone-600h.onrender.com';
   let value = {
     ServerUrl, loading, setloading
   }
