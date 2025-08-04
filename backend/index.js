e6ae5ea4bbf2b0a9bacb7f9934dc626f343b611e
Aditya Yadav <aditya828326@gmail.com>
2025-08-04 19:26:13 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const app = express()
 app.use(cors(
    {
     //  origin:'http://localhost:5173',
-     origin:'https://airbn-clone.onrender.com/',
+     origin:'https://airbn-clone.onrender.com',
     credentials: true
 }
 ))
